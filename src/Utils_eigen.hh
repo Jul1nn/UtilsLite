b6940053e7c8dbb8bfa8b6eeb9748c8534b576c9
Jul1nn <jul1nn@yandex.ru>
2026-04-17 11:49:41 +0300

    Maybe no fix needed

--- src/Utils_eigen.hh
+++ src/Utils_eigen.hh
@@ -62,16 +62,6 @@
 #include <Eigen/QR>
 #include <Eigen/Sparse>
 
-namespace Eigen
-{
-  template <typename Scalar, int Size>
-  using Vector = Matrix<Scalar, Size, 1>;
-
-  template <typename Scalar>
-  using VectorX = Matrix<Scalar, Dynamic, 1>;
-
-} // namespace Eigen
-
 #ifndef DOXYGEN_SHOULD_SKIP_THIS
 
 namespace fmt
